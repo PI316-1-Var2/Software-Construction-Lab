546553465bd6282de8874d948a67e9ee0dbfcafd
Zinkum <Zinkum@users.noreply.github.com>
2019-11-04 20:40:56 +0200

    Added Kitchen logic for Model and Control Layers

---
+++ Control/Controllers/KitchenController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Models;
+using Model.Entities;
+
+namespace Control.Controllers
+{
+    public class KitchenController : BaseController
+    {
+        public KitchenModel model;
+        public KitchenController()
+        {
+            model = new KitchenModel();
+            isActive = true;
+        }
+        public override string CheckInput(string body, string command)
+        {
+            string output = "KitchenController\n\n";
+            switch (command.ToLower())
+            {
+                case "1":
+                    output += Get(body);
+                    break;
+                case "2":
+                    output += GetAll();
+                    break;
+                case "3":
+                    output += Add(body);
+                    break;
+                case "4":
+                    output += Remove(body);
+                    break;
+                case "5":
+                    output += Change(body);
+                    break;
+                case "0":
+                    isActive = false;
+                    break;
+                case null:
+                    break;
+                default:
+                    output += "Unsupported command!";
+                    break;
+            }
+            return output + GetCommands();
+        }
+        protected override string Add(string input)
+        {
+            string output = "";
+            try
+            {
+                string[] parameters = input.Split('|');
+                Dish result = model.Add(int.Parse(parameters[0]),
+                                           parameters[1],
+                                           parameters[2]);
+                output = "Object was successfully created:\n" + "+---+--------------+--------------+\n" +
+                       string.Format(Dish.OutputPattern, "ID", "Name", "Product") +
+                       "+---+--------------+--------------+\n" +
+                       result.ToString();
+            }
+            catch (FormatException)
+            {
+                output = "Wrong input format.\nTry again.\n";
+            }
+            catch (DishException)
+            {
+                output = "Error occured while creating a dish.\nTry again.\n";
+            }
+            catch (Exception)
+            {
+                output = "Unexpected error occured while creating a dish.\nTry again.\n";
+            }
+            return output;
+        }
+
+
+        protected override string Change(string input)
+        {
+            string output = "";
+            try
+            {
+                string[] parameters = input.Split('|');
+                Dish result = model.Change(int.Parse(parameters[0]),
+                                           parameters[1],
+                                           parameters[2]);
+                output = "Object was successfully changed:\n" + "+---+--------------+--------------+\n" +   // змінити по патерну
+                       string.Format(Dish.OutputPattern, "ID", "Name", "Product") +
+                       "+---+--------------+--------------+\n" +
+                       result.ToString();
+            }
+            catch (FormatException)
+            {
+                output = "Wrong input format.\nTry again.\n";
+            }
+            catch (ProductException)
+            {
+                output = "Error occured while changing a dish.\nTry again.\n";
+            }
+            catch (Exception)
+            {
+                output = "Unexpected error occured while changing a dish.\nTry again.\n";
+            }
+            return output;
+        }
+
+        protected override string Get(string input)
+        {
+            string output = "";
+            try
+            {
+                Dish result = model.Get(int.Parse(input));
+                output = $"Dish#({input}):\n" + "+---+--------------+--------------+\n" +   // змінити по патерну
+                       string.Format(Dish.OutputPattern, "ID", "Name", "Product") +
+                       "+---+--------------+--------------+\n" +
+                       result.ToString();
+            }
+            catch (FormatException)
+            {
+                output = "Wrong input format.\nTry again.\n";
+            }
+            catch (ProductException)
+            {
+                output = "Error occured while retrieving a dish.\nTry again.\n";
+            }
+            catch (Exception)
+            {
+                output = "Unexpected error occured while retrieving a dish.\nTry again.\n";
+            }
+            return output;
+        }
+
+        protected override string GetAll()
+        {
+            string output = "";
+            try
+            {
+                List<Dish> result = model.GetAll();
+                output = "Dish:\n" + "+---+--------------+--------------+\n" +
+                       string.Format(Dish.OutputPattern, "ID", "Name", "Product") +
+                       "+---+--------------+--------------+\n";
+
+                foreach (var d in result)
+                {
+                    output += d.ToString();
+                }
+
+            }
+            catch (ProductException)
+            {
+                output = "Error occured while retrieving dishes.\nTry again.\n";
+            }
+            catch (Exception)
+            {
+                output = "Unexpected error occured while retrieving dishes.\nTry again.\n";
+            }
+            return output;
+        }
+
+        protected override string Remove(string input)
+        {
+            string output = "";
+            try
+            {
+                Dish result = model.Remove(int.Parse(input));
+                output = $"Dish#({input}):\n" + "+---+--------------+--------------+\n" +
+                       string.Format(Product.OutputPattern, "ID", "Name", "Product") +
+                       "+---+--------------+--------------+\n" +
+                       result.ToString();
+            }
+            catch (FormatException)
+            {
+                output = "Wrong input format.\nTry again.\n";
+            }
+            catch (ProductException)
+            {
+                output = "Error occured while removing a dish.\nTry again.\n";
+            }
+            catch (Exception)
+            {
+                output = "Unexpected error occured while removing a dish.\nTry again.\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ Model/Entities/Dish.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Dish
+    {
+        public int ID;
+        public string Name;
+        public string Product;
+        public const string OutputPattern = "|{0,-3}|{1,-14}|{3,-14}";
+        public Dish(int id, string name, string product)
+        {
+            ID = id;
+            Name = name;
+            Product = product;
+        }
+        public override string ToString()
+        {
+            return string.Format(OutputPattern, ID, Name, Product) + "+---+--------------+--------------+\n";
+        }
+    }
+
+    public class DishException : Exception
+    {
+        public DishException() : base()
+        {
+        }
+        public DishException(string message) : base(message)
+        {
+        }
+        public DishException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
