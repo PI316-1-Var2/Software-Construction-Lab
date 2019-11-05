41a7d8589aa07575aa95a2fb699a70938b7e15ea
Zinkum <Zinkum@users.noreply.github.com>
2019-11-05 05:13:43 +0200

    Added Logging Service

---
+++ Logging/UtcDateRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.LayoutRenderers;
+
+namespace Logging
+{
+    [LayoutRenderer("utc_date")]
+    public class UtcDateRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            builder.Append($"Time:{DateTime.UtcNow.ToString()};");
+        }
+    }
+}
